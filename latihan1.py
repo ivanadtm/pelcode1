853fa373c34ae2dabfeba49908a907c75d801131
ivanadtm <143860055+ivanadtm@users.noreply.github.com>
2023-09-02 10:59:45 +0700

    Add files via upload
    upload file

---
+++ latihan1.py
@@ -0,0 +1,38 @@
+nama_saya = "nama saya ivan"
+print(nama_saya, "bertipe", type(nama_saya))
+
+umur = 18
+print(umur, "bertipe", type(umur))
+
+IPK = 4.00
+print(IPK, "bertipe", type(IPK))
+
+menikah = False
+print(menikah, "bertipe", type(menikah))
+
+matkul_diambil = ["alpro", "sig"]
+print("Matkul : ",matkul_diambil, "bertipe", type(matkul_diambil))
+
+matkul_diambil2 = ("web","pemvis")
+print("Matkul Lain: ",matkul_diambil2, "bertipe", type(matkul_diambil2))
+
+matkul_diambil3 = {"Alpro", "Alpro", "Pemweb", "Data Science", "Data Science"}
+print("Matkul Lain-Lain: ",matkul_diambil3, "bertipe", type(matkul_diambil3))
+
+print("1"+"1")
+print(1+1)
+print("he"*3)
+print(17/3)
+print(17//3)
+print(17%3)
+
+nama_saya=str(input("masukan nama kamu = "))
+print(nama_saya, "bertipe", type(nama_saya))
+
+umur=int(input("masukan umur kamu = "))
+print(umur, "bertipe", type(umur))
+
+IPK=float(input("masukan IPK kamu = "))
+print(IPK, "bertipe", type(IPK))
+
+
